d7f37009d46e70a600c3f566a03928c436db8898
Thomas Sutter <t.sutter@me.com>
2019-11-06 14:24:42 +0100

    bugfix: pathnames in preprocessing

--- preprocessing/ColumnSplitter.py
+++ preprocessing/ColumnSplitter.py
@@ -111,10 +111,11 @@ class ColumnSplitter:
     def splitColumnsAllSubgroups(self):
         dir_data = self.options.getDirData();
         dataset = self.options.getDatasetName();
+        prefix = self.options.getDataPrefix();
         for g in self.options.getSubgroups():
-            filename_out = os.path.join(dir_data, 'data_' + dataset + '_' + g + '.csv');
+            filename_out = os.path.join(dir_data, prefix + '_' + dataset + '_' + g + '.csv');
             self.splitColumns(g, filename_out);
-        filename_out_rest = os.path.join(dir_data, 'data_' + dataset + '_REST.csv');
+        filename_out_rest = os.path.join(dir_data, prefix + '_' + dataset + '_REST.csv');
         self.splitColumns('REST', filename_out_rest);
 
 
